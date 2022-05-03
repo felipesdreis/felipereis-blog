b18903c5da320288fa741b235ec06d5afc4e546b
FelipeReis <felipe.reis@icarotech.com>
2022-05-03 00:43:23 -0300

    new contect-type post

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # felipereis-blog
- strapi-cms-blog
+
+CMS para o site [felipedosreis](felipedosreis.com.br)
+
+### Open Source Project
+
+[Strapi](https://github.com/strapi/strapi)
+
+### Local run dev
+
+> npm run develop
+
+Necessário rodar como develop para criar novos Content-Type
--- config/server.js
+++ config/server.js
@@ -1,16 +1,16 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
-
-
 module.exports = ({ env }) => ({
-  url: env("https://felipereis-blog-cms.herokuapp.com/"),
-  proxy: true,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array('APP_KEYS'),
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   url: env("https://felipereis-blog-cms.herokuapp.com/"),
+//   proxy: true,
+//   app: {
+//     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+//   },
+// });
